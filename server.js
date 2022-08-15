c0fac89844e73e3944ca23c83ddb84889c9309f0
Emilia Åslund <92573664+emiliaaslund@users.noreply.github.com>
2022-08-15 10:30:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Goodbye World!");
+  res.send("Hello World, it's a beautiful dayyyy !");
 });
 
 app.listen(PORT, () => {
